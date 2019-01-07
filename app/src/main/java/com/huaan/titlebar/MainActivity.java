7c691ff119fe49f4666a5ddf9c687467266a02e2
renlei <572159012@qq.com>
2019-01-07 18:10:04 +0800

    first

---
+++ app/src/main/java/com/huaan/titlebar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.huaan.titlebar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.library.titlebarlibrary.TitleBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TitleBar mTitleBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTitleBar = (TitleBar) findViewById(R.id.title_bar);
+        //点击事件
+        mTitleBar.setOnTitleBarClickListener(new TitleBar.OnTitleBarClickListener() {
+            @Override
+            public void onLeftBtnClick(View view) {
+
+            }
+
+            @Override
+            public void onRightBtnClick(View view) {
+
+            }
+
+            @Override
+            public void onTitleClick(View view) {
+
+            }
+        });
+
+//        或则
+        mTitleBar.getImg_left().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
