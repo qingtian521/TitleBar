3be6ae390cbce586a9e05e79ace094870f7b9bca
renlei <572159012@qq.com>
2019-01-08 17:09:33 +0800

    优化了沉浸式效果

--- titlebarlibrary/src/main/java/com/library/titlebarlibrary/TitleBar.java
+++ titlebarlibrary/src/main/java/com/library/titlebarlibrary/TitleBar.java
@@ -22,6 +22,7 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -30,6 +31,14 @@ import android.widget.TextView;
  * create 2019/01/07
  */
 public class TitleBar extends FrameLayout implements View.OnClickListener {
+
+    /**
+     * 沉浸式状态属性
+     * IMMERSIVE_TRANSPARENT 全部透明
+     * IMMERSIVE_TRANSLUCENT 半透明
+     */
+    private static final int IMMERSIVE_TRANSPARENT = 0x01;
+    private static final int IMMERSIVE_TRANSLUCENT = 0x02;
 
     /**
      *  中间标题
@@ -341,23 +350,66 @@ public class TitleBar extends FrameLayout implements View.OnClickListener {
     /**
      * 开启沉浸式
      * @param activity 当前activity
+     * @param flag:IMMERSIVE_TRANSPARENT 表示全透明状态 or IMMERSIVE_TRANSLUCENT 半透明状态 ,仅6.0以上可以生效
+     */
+    public void startImmersive(Activity activity,int flag) {
+        setColor(activity,backgroundColor,flag);
+    }
+
+    /**
+     * 开启沉浸式
+     * @param activity 当前activity
      */
     public void startImmersive(Activity activity) {
-        //android 6.0 以上
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        setColor(activity,backgroundColor,IMMERSIVE_TRANSPARENT);
+    }
+
+    /**
+     * 设置状态栏颜色
+     * @param activity 需要设置的activity
+     * @param color    状态栏颜色值
+     */
+    public void setColor(Activity activity, int color,int flag) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            activity.getWindow().getDecorView().setSystemUiVisibility(option);
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            activity.getWindow().setStatusBarColor(backgroundColor);
-        }
-        //android 4.4 -- 6.0
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if(flag == IMMERSIVE_TRANSPARENT){
+                activity.getWindow().setStatusBarColor(backgroundColor);
+            }
+        }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 设置状态栏透明
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //添加一个占位View
-            View view = new View(getContext());
-            view.setBackgroundColor(backgroundColor);
-            LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,50);
-            ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
-            decorView.addView(view,params);
         }
+        // 生成一个状态栏大小的矩形
+        View statusView = createStatusView(activity, color);
+        // 添加 statusView 到布局中
+        ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+        decorView.addView(statusView);
+        // 设置根布局的参数
+        ViewGroup rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+        rootView.setFitsSystemWindows(true);
+        rootView.setClipToPadding(true);
+    }
+
+    /**
+     * 生成一个和状态栏大小相同的矩形条
+     * @param activity 需要设置的activity
+     * @param color    状态栏颜色值
+     * @return 状态栏矩形条
+     */
+
+    private View createStatusView(Activity activity, int color) {
+        // 获得状态栏高度
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+        // 绘制一个和状态栏一样高的矩形
+        View statusView = new View(activity);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                statusBarHeight);
+        statusView.setLayoutParams(params);
+        statusView.setBackgroundColor(color);
+        return statusView;
     }
 }
